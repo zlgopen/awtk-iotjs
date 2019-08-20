7c1bed2ca384568e3dbb76ed78207a555435a737
lixianjing <xianjimli@hotmail.com>
2019-08-19 20:32:12 -0700

    update build scripts

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 source config.env
 
-${IOTJS_ROOT}/tools/build.py --external-modules=${CWD}/awtk-module --cmake-param=-DENABLE_MODULE_AWTK=ON
+${IOTJS_ROOT}/tools/build.py --external-modules=${CWD}/awtk-module --cmake-param=-DENABLE_MODULE_AWTK=ON --cmake-param=-DMEM_HEAP_SIZE_KB=4906
 
 
